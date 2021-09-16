6a74cd7285cc2a26d894f1d5a0cd315a09fe5927
Neoplatonist <mr.neoplatonist@gmail.com>
2021-09-16 15:16:45 +0900

    cleaned components/Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,13 @@
-// import CategoryButtons from "./CategoryButtons"
-import { Card, Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import Footer from "./Footer"
 import Navbar from "./Navbar"
 
-const Layout = ({ children, categories }) => {
+const Layout = ({ children }) => {
   return (
     <Container fluid>
       <Row>
         <Navbar />
 
-        {/* <CategoryButtons categories={categories} /> */}
         <Col>{children}</Col>
 
         <Footer />
@@ -19,7 +17,3 @@ const Layout = ({ children, categories }) => {
 }
 
 export default Layout
-
-// className="flex justify-center bg-gray-200"
-// className="max-w-screen-lg flex flex-col min-h-screen w-full"
-//  className="flex-grow"
